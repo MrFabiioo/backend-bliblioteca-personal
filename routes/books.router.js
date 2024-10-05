679b6c483758c17287dc883217a1cf563da3d4db
MrFabiioo <fabiolin57@gmail.com>
2024-10-05 11:34:47 -0500

    book-router and initial book-service

---
+++ routes/books.router.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const BooksService = require('../services/book.service')
+
+const services = new BooksService();
+//--- get books
+
+router.get('/',async (req,res)=>{
+  const books = await services.find();
+  res.json(books);
+});
+
+//-- get books for Id
+router.get('/:id', async(req,res,next)=>{
+  try {
+    const {id}= req.params;
+    const book = await services.findOne(id);
+    res.json(book);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//-- create books
+
+router.post('/',async(req,res,next)=>{
+  try {
+    const body = req.body;
+    const newBook = await services.create(body);
+    res.status(201).json(newBook);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//---- update book with Id
+
+router.patch('/:id', async(req, res,next)=>{
+  try {
+    const {id}= req.params;
+    const body = req.body;
+    const book = await services.update(id,body);
+    res.json(book);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//--- delete book
+
+router.delete('/:id', async(req,res,next)=>{
+  try {
+    const {id}= req.params;
+    const book = await services.delete(id);
+    res.json(book);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.export = router;
---
+++ services/book.service.js
@@ -0,0 +1,28 @@
+
+class BooksService {
+  constructor(){
+    this.books = [];
+  }
+
+  async create(data){
+
+  }
+
+  async find (){
+
+  }
+
+  async findOne(id){
+
+  }
+
+  async update(id, changes){
+
+  }
+
+  async delete(id){
+
+  }
+}
+
+module.export =  BooksService;
