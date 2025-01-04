b7852c7830e7e8a0de500807ff1bdd0929d889a4
Fabio Ortega <fabiolin57@gmail.com>
2025-01-04 10:51:50 -0500

    middleware errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const {errorHandler,logErros,boomErrorHandler} = require('./middlewares/error.handler');
 const routerApi = require('./routes')
 const app = express();
 const port = process.env.PORT;
@@ -11,9 +12,14 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log('Puerto ' +  port + ' Funcionando');
-});
+
 
 routerApi(app);
 
+app.use(logErros);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Puerto ' +  port + ' Funcionando');
+});
