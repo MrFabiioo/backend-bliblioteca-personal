d1e7f5e438023a3b6b5a391b1010497fe5f5a159
Fabio Ortega <fabiolin57@gmail.com>
2025-02-26 23:05:52 -0500

    quité el validatorHandler de category para terminar pruebas

--- routes/category.router.js
+++ routes/category.router.js
@@ -37,11 +37,9 @@ router.get('/:id',jwtCheck,checkScopes,
 );
 
 router.post('/',jwtCheck,checkScopes,
-  validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
       const body = req.body;
-      console.log('Cuerpo recibido:', body); // Agregar para debug
       const newCategory = await service.create(body);
       res.status(201).json(newCategory);
     } catch (error) {
