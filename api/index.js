4d2c8a8bfcee9139fa95b9f9a6d4d28843aa5db2
Fabio Ortega <fabiolin57@gmail.com>
2025-03-11 11:03:42 -0500

    agregando dominio de acceso por cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 
-const whitelist = ['http://localhost:3000', 'https://myapp.co','http://localhost:5173'];
+const whitelist = ['https://manager-library.vercel.app','http://localhost:5173'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
